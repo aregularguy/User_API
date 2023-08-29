f6920d78044c49193754acbc4eb9e8f1674e0eb9
arshad <arshad@gmail.com>
2023-08-29 13:40:46 +0530

    commited all files

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-app.use(express.static('client/build'));
+app.use(express.static('client2/build'));
  
 const path = require('path');
 app.get('*', (req, res) => {
-res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+res.sendFile(path.resolve(__dirname, 'client2', 'build', 'index.html'));
 });
 const PORT = process.env.PORT || 5000;
 console.log('server started on port:',PORT);
